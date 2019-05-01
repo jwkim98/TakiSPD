af7907a1c0d1f45c00f825fff58849f5e4cb8674
jwkim98 <jwkim98@kaist.ac.kr>
2019-05-01 15:19:03 +0900

    fix: resolve error

--- SourceCode/Model/Main.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-
-def main():
-    print(tf.__version__)
-
-
-if name == '__main__':
-    main()
--- src/Model/Main.py
+++ src/Model/Main.py
@@ -5,5 +5,5 @@ def main():
     print(tf.__version__)
 
 
-if name == '__main__':
+if __name__ == '__main__':
     main()
