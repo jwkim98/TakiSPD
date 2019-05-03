34eaeb2ba85f382d5714f37a483c9138946737a7
jwkim98 <jwkim98@kaist.ac.kr>
2019-05-04 00:30:40 +0900

    refactor: cleaning the code

--- src/Model/main.py
+++ src/Model/main.py
@@ -1,22 +1,18 @@
 import tensorflow as tf
 import os
 from word2vec import gensim_word2vec as word_vec
-from gensim.models import KeyedVectors
 
 
 def main():
     print(tf.__version__)
+    path = os.path.join(os.path.curdir, 'word2vec', 'TrainedModel')
+    word_vec.save_trained_model(path)
     load_gensim_model_example()
 
 
-def get_gensim_model():
-    print('Downloading pretrained model')
-    model = word_vec.get_trained_model()
-    model.wv.save(os.path.join(os.path.curdir, 'word2vec', 'TrainedModel', 'word2vec.kv'))
-
-
 def load_gensim_model_example():
-    loaded_model = KeyedVectors.load(os.path.join(os.path.curdir, 'word2vec', 'TrainedModel', 'word2vec.kv'))
+    filename = os.path.join(os.path.curdir, 'word2vec', 'TrainedModel', 'word2vec.kv')
+    loaded_model = word_vec.load_keyVector(filename)
     vector = loaded_model['sale']
     print(vector)
 
--- src/Model/word2vec/gensim_word2vec.py
+++ src/Model/word2vec/gensim_word2vec.py
@@ -3,15 +3,14 @@ import sys
 import gensim
 import subprocess
 from keras.utils import get_file
-import numpy as np
 
 
 # Download pre-trained gensim model
-def get_trained_model():
+def save_trained_model(save_path):
     MODEL = 'GoogleNews-vectors-negative300.bin'
-
+    # get trained model by google word2vec
     path = get_file(MODEL + '.gz', 'https://deeplearning4jblob.blob.core.windows.net/resources/wordvectors/%s.gz' % MODEL)
-    unzipped = os.path.join(os.path.curdir, 'word2vec', 'TrainedModel',  MODEL)
+    unzipped = os.path.join(save_path,  MODEL)
     print(unzipped)
     if not os.path.isfile(unzipped):
         with open(unzipped, 'wb') as fout:
@@ -19,8 +18,14 @@ def get_trained_model():
             zcat = subprocess.Popen(['zcat'], stdin=open(path), stdout=fout, stderr=sys.stderr)
             zcat.wait()
 
-    model = gensim.models.KeyedVectors.load_word2vec_format(unzipped, binary=True)
-    return model
+    key_vector = gensim.models.KeyedVectors.load_word2vec_format(unzipped, binary=True)
+    key_vector.wv.save(os.path.join(save_path, 'word2vec.kv'))
+    return key_vector
+
+
+def load_keyVector(filename):
+    loaded_model = gensim.models.KeyedVectors.load(filename)
+    return loaded_model
 
 
 def get_word_vector(model, word):
